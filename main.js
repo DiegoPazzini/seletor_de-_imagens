31c5be3a59311197a7f30383ab9bde82ca8a8a26
diego.pazzini2009@hotmail.com <diegopazzini2009@hotmail.com>
2023-07-11 02:01:52 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown()
+    })
+
+    $('#cancel').click(function(){
+        $('form').slideUp()
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const inputUrlImg = $('#urlImage').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src='${inputUrlImg}'/>`).appendTo(novoItem);
+        $(
+            `<div class='imgLink'
+                <a href="${inputUrlImg}" target="_blank title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+                </a>
+            </div>`
+        ).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000); //(1000 = 1 segundo)
+        $('#urlImage').val('');
+    })
+
+})
